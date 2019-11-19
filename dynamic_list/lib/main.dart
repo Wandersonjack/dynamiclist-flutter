e359b9183cc9528dc392418c3b43df366d2c92e9
wandersonjack <wander8jackson@gmail.com>
2019-11-19 18:10:02 +0200

    fetching data from api

--- dynamic_list/lib/main.dart
+++ dynamic_list/lib/main.dart
@@ -1,13 +1,103 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'Photo streamer',
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: PhotoList(),
+    );
+  }
 }
 
+class PhotoList extends StatefulWidget {
+  @override
+  _PhotoListState createState() => _PhotoListState();
+}
+
+class _PhotoListState extends State<PhotoList> {
+  StreamController<Photo> streamController;
+  List<Photo> list = [];
+
+  @override
+  void initState() {
+    super.initState();
+    streamController = StreamController.broadcast();
+
+    streamController.stream.listen((p) => setState(() => list.add(p)));
+
+    load(streamController);
+  }
+
+  load(StreamController<Photo> sc) async {
+    String url = "https://jsonplaceholder.typicode.com/photos";
+    var client = new http.Client();
+
+    var req = new http.Request('get', Uri.parse(url));
+
+    var streamedRes = await client.send(req);
+
+    streamedRes.stream
+        .transform(utf8.decoder)
+        .transform(json.decoder)
+        .expand((e) => e)
+        .map((map) => Photo.fromJsonMap(map))
+        .pipe(sc);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    streamController?.close();
+    streamController = null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Photo stream"),
+      ),
+      body: Center(
+        child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          itemBuilder: (BuildContext context, int index) => _makeElement(index),
+        ),
+      ),
+    );
+  }
+
+  Widget _makeElement(int index) {
+    //checking the list because its a dynamic sized
+    if (index >= list.length) {
+      return null;
+    }
+
+    return Container(
+      padding: EdgeInsets.all(5.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(list[index].url, width: 300.0, height: 300.0,),
+          Text(list[index].title)
+        ],
+      ),
+    );
+  }
+}
 
 //model
 class Photo {
+  final String title;
+  final String url;
 
+  Photo.fromJsonMap(Map map)
+      : title = map['title'],
+        url = map['url'];
 }
